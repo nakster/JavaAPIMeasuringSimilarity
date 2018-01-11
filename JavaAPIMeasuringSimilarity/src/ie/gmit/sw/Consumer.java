4716f56c795e87bf091e002537791c252ac1ea7e
nakster <g00332403@gmit.ie>
2018-01-11 00:28:47 +0000

    Consumer added

--- JavaAPIMeasuringSimilarity/src/ie/gmit/sw/Consumer.java
+++ JavaAPIMeasuringSimilarity/src/ie/gmit/sw/Consumer.java
@@ -1,5 +1,87 @@
 package ie.gmit.sw;
 
-public class Consumer {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import ie.gmit.sw.Poison;
 
+public class Consumer implements Runnable {
+
+	private BlockingQueue<Shingle> bQueue;
+	private int numOfMinH;
+	private int[] minhashes;
+	private ConcurrentMap<Integer,List<Integer>> map = new ConcurrentHashMap<Integer, List<Integer>>();
+	private ExecutorService pool;
+
+	//gets Map
+	public ConcurrentMap<Integer, List<Integer>> getMap() {
+		return map;
+	}
+	//constructor 
+	public Consumer(BlockingQueue<Shingle> bQueue, int numOfMinH, int poolSize) {
+		this.bQueue = bQueue;
+		this.numOfMinH = numOfMinH;
+		pool = Executors.newFixedThreadPool(poolSize);
+		init();
+	}
+	//intialises specified number of minhashes 
+	public void init() {
+		Random random = new Random();
+		minhashes = new int[numOfMinH];
+		for(int i=0; i < minhashes.length; i++) {
+			minhashes[i] = random.nextInt();
+		}
+	}
+
+	public void run() {
+		int docCount = 2;
+		while(docCount > 0) {
+			try {
+				Shingle s = bQueue.take();
+				if(s instanceof Poison) {
+					docCount--;
+				} else {
+					pool.execute(new Runnable() {
+						@Override
+						public void run() {
+
+							List<Integer>list = map.get(s.getDocId());
+							for(int i=0;i<minhashes.length;i++) {
+								int value = s.getHashCode() ^ minhashes[i];
+								list = map.get(s.getDocId());
+								if(list == null) {
+									list = new ArrayList<Integer>(Collections.nCopies(numOfMinH, Integer.MAX_VALUE));
+									map.put(s.getDocId(),list);
+
+								}
+								else {		
+									if(list.get(i)>value) {
+										list.set(i, value);
+									}
+								}
+							} 
+							map.put(s.getDocId(), list);			
+						}
+					});
+				}
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		List<Integer> intersection = map.get(1);
+		intersection.retainAll(map.get(2));
+		//the calculation which calculates the percentage 
+		float jacquared = (float)intersection.size()/(numOfMinH*2-(float)intersection.size());
+		
+		System.out.println("It Matches: " + (jacquared) * 100 + " %");
+	}
 }
