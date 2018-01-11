cccd188ce6ea80156472e8dcb005137663a7de19
nakster <g00332403@gmit.ie>
2018-01-11 00:42:39 +0000

    added menu

--- JavaAPIMeasuringSimilarity/src/ie/gmit/sw/Menu.java
+++ JavaAPIMeasuringSimilarity/src/ie/gmit/sw/Menu.java
@@ -1,7 +1,35 @@
 package ie.gmit.sw;
 
+import java.util.Scanner;
+
 public class Menu {
 	
 	Launch l = new Launch();
-
+	
+	private Scanner console = new Scanner(System.in);
+	
+	public Menu() throws InterruptedException {
+		
+			//ask the user following questions 
+			System.out.println("Please Enter The Name Of File One: ");
+			String f1 = console.next();
+			
+			System.out.println("Please Enter The Name Of File Two: ");
+			String f2 = console.next();
+			
+			System.out.println("Please Enter Shingle size: ");
+			int shingleSize = console.nextInt();
+			
+			System.out.println("Please Enter the Size for Number of Min Hashes: ");
+			int NumOfMin = console.nextInt();
+			
+			System.out.println("Enter pool size: ");
+			int pool = console.nextInt();
+			
+			//send the info to the launch class which then runs the threads 			
+			l.StartThreads(f1, f2, shingleSize, NumOfMin, pool);
+		
+		
+	}
+	
 }
