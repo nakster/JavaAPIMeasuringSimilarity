bbdbb3aef507184cefb11b2daa81817282050bc4
nakster <g00332403@gmit.ie>
2018-01-11 00:05:54 +0000

    Added Shingle class variables

--- JavaAPIMeasuringSimilarity/src/ie/gmit/sw/Shingle.java
+++ JavaAPIMeasuringSimilarity/src/ie/gmit/sw/Shingle.java
@@ -1,5 +1,35 @@
 package ie.gmit.sw;
 
 public class Shingle {
+	//variables 
+	private int docId;
+	private int hashCode;
+	
+	//Constructors 
+	public Shingle() {
+		super();
+	}
+
+	public Shingle(int docId, int hashCode) {
+		super();
+		this.docId = docId;
+		this.hashCode = hashCode;
+	}
+	//getters and setters 
+	public int getDocId() {
+		return docId;
+	}
+
+	public void setDocId(int docId) {
+		this.docId = docId;
+	}
+
+	public int getHashCode() {
+		return hashCode;
+	}
+
+	public void setHashCode(int hashCode) {
+		this.hashCode = hashCode;
+	}
 
 }
